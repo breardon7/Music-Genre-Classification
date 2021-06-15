9852befd6541da7f2877fb3e186ee7cf1edd38f5
Haruna Salim <salxsaa@gmail.com>
2021-06-15 18:32:26 -0400

    Bug Fixes

--- music.py
+++ music.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-
 from music_features import convert_mp3_to_wav, print_label
 
 sns.set(color_codes=True)
@@ -20,15 +19,19 @@ df1['label'].unique()
 df1['label'].value_counts(normalize=True)
 X_train, X_test, y_train, y_test = train_test_split(df1.drop('label', axis=1), df1['label'], test_size=.3,
                                                     random_state=22)
+
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
-mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=10000)
+mlp = MLPClassifier(hidden_layer_sizes=(1, 2, 1), max_iter=10000)
 mlp.fit(X_train, y_train)
-data = convert_mp3_to_wav("changes.mp3")
-prediction = mlp.predict(data)
-y_pred_report = np.full(len(y_train), prediction)
-report = classification_report(y_pred_report, y_train);
+data = convert_mp3_to_wav("ucl_anthem.mp3")
+predictions = mlp.predict(data)
+u, c = np.unique(predictions, return_counts=True)
+prediction = u[c == c.max()]
+print(prediction)
+# y_pred_report = np.full(len(y_train), prediction)
+#report = classification_report(predictions, y_train);
 print(print_label(prediction))
-print(classification_report(y_pred_report, y_train))
+# print(classification_report(y_pred_report, y_train))
--- music_features.py
+++ music_features.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import librosa as librosa
 import numpy as np
 import pandas as pd
@@ -5,46 +8,47 @@ from pydub import AudioSegment
 
 
 def convert_mp3_to_wav(filepath):
-    print(filepath)
+    letters = string.ascii_lowercase
+    name = ''.join(random.choice(letters) for i in range(5))
+    wav_file_name = name + ".wav"
     song = AudioSegment.from_mp3(filepath)
-    song.export("now.wav", format="wav")
-    return wav_features("now.wav")
+    song.export(wav_file_name, format="wav")
+    return wav_features(wav_file_name)
 
 
 def wav_features(filepath):
     y, sr = librosa.load(filepath)
-    x, sr = librosa.load(filepath)
-    chroma_stft = np.array(librosa.feature.chroma_stft(y=y, sr=sr)[0]).mean()
-    rmse = np.array(librosa.feature.rms(y=y)[0]).mean()
-    spectral_centroid = np.array(librosa.feature.spectral_centroid(x, sr=sr)[0]).mean()
-    spectral_bandwidth = np.array(librosa.feature.spectral_bandwidth(y=y, sr=sr)).mean()
-    rolloff = np.array(librosa.feature.spectral_rolloff(y=y, sr=sr)).mean()
-    zero_crossing_rate = np.array(librosa.feature.zero_crossing_rate(y)).mean()
+    chroma_stft = np.array(librosa.feature.chroma_stft(y=y, sr=sr)[0])
+    rmse = np.array(librosa.feature.rms(y=y)[0])
+    spectral_centroid = np.array(librosa.feature.spectral_centroid(y, sr=sr)[0])
+    spectral_bandwidth = np.array(librosa.feature.spectral_bandwidth(y=y, sr=sr))
+    rolloff = np.array(librosa.feature.spectral_rolloff(y=y, sr=sr))
+    zero_crossing_rate = np.array(librosa.feature.zero_crossing_rate(y))
     mfcc_features = librosa.feature.mfcc(y=y, sr=sr)
     data = pd.DataFrame(
         {"chroma_stft": chroma_stft.flatten(), "rmse": rmse.flatten(), "spectral_centroid": spectral_centroid.flatten(),
          "spectral_bandwidth": spectral_bandwidth.flatten(), "rolloff": rolloff.flatten(),
          "zero_crossing_rate": zero_crossing_rate.flatten(),
-         "mcff1": np.array(mfcc_features[0]).flatten().mean(),
-         "mcff2": np.array(mfcc_features[1]).flatten().mean(),
-         "mcff3": np.array(mfcc_features[2]).flatten().mean(),
-         "mcff4": np.array(mfcc_features[3]).flatten().mean(),
-         "mcff5": np.array(mfcc_features[4]).flatten().mean(),
-         "mcff6": np.array(mfcc_features[5]).flatten().mean(),
-         "mcff7": np.array(mfcc_features[6]).flatten().mean(),
-         "mcff8": np.array(mfcc_features[7]).flatten().mean(),
-         "mcff9": np.array(mfcc_features[8]).flatten().mean(),
-         "mcff10": np.array(mfcc_features[9]).flatten().mean(),
-         "mcff11": np.array(mfcc_features[10]).flatten().mean(),
-         "mcff12": np.array(mfcc_features[11]).flatten().mean(),
-         "mcff13": np.array(mfcc_features[12]).flatten().mean(),
-         "mcff14": np.array(mfcc_features[13]).flatten().mean(),
-         "mcff15": np.array(mfcc_features[14]).flatten().mean(),
-         "mcff16": np.array(mfcc_features[15]).flatten().mean(),
-         "mcff17": np.array(mfcc_features[16]).flatten().mean(),
-         "mcff18": np.array(mfcc_features[17]).flatten().mean(),
-         "mcff19": np.array(mfcc_features[18]).flatten().mean(),
-         "mcff20": np.array(mfcc_features[19]).flatten().mean(),
+         "mcff1": np.array(mfcc_features[0]).flatten(),
+         "mcff2": np.array(mfcc_features[1]).flatten(),
+         "mcff3": np.array(mfcc_features[2]).flatten(),
+         "mcff4": np.array(mfcc_features[3]).flatten(),
+         "mcff5": np.array(mfcc_features[4]).flatten(),
+         "mcff6": np.array(mfcc_features[5]).flatten(),
+         "mcff7": np.array(mfcc_features[6]).flatten(),
+         "mcff8": np.array(mfcc_features[7]).flatten(),
+         "mcff9": np.array(mfcc_features[8]).flatten(),
+         "mcff10": np.array(mfcc_features[9]).flatten(),
+         "mcff11": np.array(mfcc_features[10]).flatten(),
+         "mcff12": np.array(mfcc_features[11]).flatten(),
+         "mcff13": np.array(mfcc_features[12]).flatten(),
+         "mcff14": np.array(mfcc_features[13]).flatten(),
+         "mcff15": np.array(mfcc_features[14]).flatten(),
+         "mcff16": np.array(mfcc_features[15]).flatten(),
+         "mcff17": np.array(mfcc_features[16]).flatten(),
+         "mcff18": np.array(mfcc_features[17]).flatten(),
+         "mcff19": np.array(mfcc_features[18]).flatten(),
+         "mcff20": np.array(mfcc_features[19]).flatten(),
          }
     )
     return data
