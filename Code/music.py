f3b83677692930532ed7cb6ef7f374b0a77cae22
Haruna Salim <salxsaa@gmail.com>
2021-06-15 21:09:37 -0400

    Updated The classification Report

--- Code/music.py
+++ Code/music.py
@@ -26,38 +26,40 @@ scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
-print("============PREDICT TEST SPLIT WITH MLP CLASSIFIER=====================")
 mlp = MLPClassifier(hidden_layer_sizes=(10, 20, 10), max_iter=10000, learning_rate='adaptive', solver="sgd",
                     activation="relu")
+
+print("============PREDICT TEST SPLIT WITH MLP CLASSIFIER=====================")
 mlp.fit(X_train, y_train)
-prediction_data = convert_mp3_to_wav("Music_Sample/ucl_anthem.mp3")
 x_predictions = mlp.predict(X_test)
 print(classification_report(y_test, x_predictions))
+print(confusion_matrix(y_test, x_predictions))
 
 
-
-print("============PREDICT CUSTOM MP3 FILE WITH MLP CLASSIFIER=====================")
+features = convert_mp3_to_wav("Music_Sample/ucl_anthem.mp3")
+prediction_data = features.iloc[150:]
+print("============PREDICT UPLOADED MP3 FILE WITH MLP CLASSIFIER=====================")
 mlp = MLPClassifier(hidden_layer_sizes=(10, 20, 10), max_iter=10000, learning_rate='adaptive', solver="sgd",
                     activation="relu")
 mlp.fit(X_train, y_train)
-prediction_data = convert_mp3_to_wav("Music_Sample/ucl_anthem.mp3")
 predictions = mlp.predict(prediction_data)
-best_prediction = predictions[np.argsort(predictions)[0]]
+best_prediction = predictions[np.argsort(predictions, axis=None)[0]]
 best_predictions = np.argsort(predictions)[0:len(y_test)]
 prediction_report = []
 for i in best_predictions:
     prediction_report.append(predictions[i])
 print(print_label(best_prediction))
 print(classification_report(y_test, prediction_report))
+print(confusion_matrix(y_test, prediction_report))
 
-
-print("============DECISION TREE=====================")
+print("============PREDICT UPLOADED MPS WITH SKLEARN DECISION TREE=====================")
 clf = DecisionTreeClassifier(criterion="gini")
 clf.fit(X_train, y_train)
 predictions2 = clf.predict(prediction_data)
-best_prediction2 = predictions2[np.argsort(predictions2)[0]]
+best_prediction2 = predictions2[np.argsort(predictions2, axis=None)[0]]
 best_predictions2 = np.argsort(predictions2)[0:len(y_test)]
 prediction_report2 = []
 for i in best_predictions2:
     prediction_report2.append(predictions2[i])
 print(classification_report(y_test, prediction_report2))
+print(confusion_matrix(y_test, prediction_report2))
